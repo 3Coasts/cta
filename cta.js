5b226837159c06be29b6c5b78a18f0412a6e7cb8
Dan Horne <dan@3coasts.com>
2013-07-28 11:12:19 -0400

    merged in FCC changes

---
+++ cta.js
@@ -0,0 +1,14 @@
+// (function ($) {
+
+
+// Drupal.behaviors.cta = {
+//   attach: function (context, settings) {
+    $("#cta-admin-settings .delete").click(function(){
+      if(!confirm("Are you sure you want to delete this CTA? This can not be undone.")){
+        return false;
+      }
+    });
+//   }
+// };
+
+// }(jQuery));
